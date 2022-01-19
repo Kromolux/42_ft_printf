025bb245392fb81cacdb9f41e4ac79cd53b4ec83
Rene Kaufmann <rkaufman@student.42wolfsburg.de>
2022-01-19 07:37:16 +0100

    another change 2022/01/19

--- ft_apply_precision_bonus.c
+++ ft_apply_precision_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rkaufman <rkaufman@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 16:58:53 by rkaufman          #+#    #+#             */
-/*   Updated: 2022/01/18 21:24:50 by rkaufman         ###   ########.fr       */
+/*   Updated: 2022/01/19 07:31:29 by rkaufman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,11 @@ char	*ft_apply_precision(char *s, t_flags *flags)
 	s_len = (int) ft_strlen(s);
 	if (flags->conversion == 's')
 	{
-		if (s_len >= flags->precision)
+		if (LINUX && ft_strncmp(s, "(null)", 7) == 0
+			&& s_len > flags->precision)
+			s[0] = '\0';
+		else if (s_len >= flags->precision)
 			s[flags->precision] = '\0';
-		//else if (flags->precision < s_len && ft_strncmp(s, "(null)", 6) == 0)
-		//	s[0] = '\0';
 	}
 	else if (ft_strchr("diuxX", flags->conversion))
 		s = ft_create_precision(s, flags, s_len);
